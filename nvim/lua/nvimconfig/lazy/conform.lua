3dabc199a13acae36d40e33a2687d4e7d4a9af48
Robert Herbst <rob@robertfall.com>
2026-01-05 15:22:30 +0200

    Wezterm tweaks etc

---
+++ nvim/lua/nvimconfig/lazy/conform.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true })
+				end,
+				desc = "Format buffer",
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				json = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				markdown = { "prettier" },
+			},
+			format_on_save = false,
+		},
+	},
+}
--- nvim/lua/nvimconfig/lazy/treesitter.lua
+++ nvim/lua/nvimconfig/lazy/treesitter.lua
@@ -16,6 +16,7 @@ return {
 				ensure_installed = {
 					"javascript",
 					"typescript",
+					"tsx",
 					"ruby",
 					"rust",
 					-- The five parsers below should always be installed
