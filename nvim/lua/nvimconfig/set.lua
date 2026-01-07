3d4c1e40576bbf5b54ff73b4d6e8469fabb019a5
Robert Herbst <rob@robertfall.com>
2026-01-07 16:49:44 +0200

    Add Windows cliboard support

--- nvim/lua/nvimconfig/set.lua
+++ nvim/lua/nvimconfig/set.lua
@@ -20,6 +20,22 @@ vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
 vim.opt.undofile = true
 vim.opt.clipboard = "unnamed"
 
+-- WSL clipboard using Windows clip.exe
+if vim.fn.has("wsl") == 1 then
+	vim.g.clipboard = {
+		name = "WslClipboard",
+		copy = {
+			["+"] = "clip.exe",
+			["*"] = "clip.exe",
+		},
+		paste = {
+			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		},
+		cache_enabled = 0,
+	}
+end
+
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
